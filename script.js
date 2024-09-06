3c74d780987968f9c5a4a278e6cc93a668fdfcb4
dharmesh-joshi <dharmeshjoshi2004@gmail.com>
2024-09-07 00:51:58 +0530

    svg in folder

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-console.log("let write a java Script");
 
-async function main() {
-    let a = await fetch("http://127.0.0.1:5500/songs/");
+
+async function getSongs() {
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs
 }
 
-main();
+async function main() {
+    // Get the list of all songs
+    let songs = await getSongs()
+
+    // Play the first songs
+    var audio = new Audio('songs');
+    audio.play();
+}
+
+main()
+
